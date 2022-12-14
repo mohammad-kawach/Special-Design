a3e97621018591870c6e6f24e2e6006e61393b1c
unknown <mohmad.kawach.777@gmail.com>
2022-12-14 19:04:45 +0300

    full responsive

--- js/master.js
+++ js/master.js
@@ -304,3 +304,36 @@ document.querySelector(".reset-options").onclick = function() {
 
   window.location.reload();
 };
+
+// toggle menu
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks    = document.querySelector(".links");
+
+toggleBtn.onclick = function(e) {
+  e.stopPropagation();
+
+  // toggle class "menu-active" on button
+  this.classList.toggle("menu-active");
+
+  // toggle class "open" on links
+  tLinks.classList.toggle("open");
+};
+
+// click anywhere  outside menu and toggle button
+document.addEventListener("click", (e) => {
+  if(e.target !== toggleBtn && e.target !== tLinks) {
+    // check if menu if open
+    if(tLinks.classList.contains("open")) {
+      // toggle class "menu-active" on button
+      toggleBtn.classList.toggle("menu-active");
+
+      // toggle class "open" on links
+      tLinks.classList.toggle("open");
+    }
+  }
+});
+
+// stop propagition on the menu
+tLinks.onclick = function(e) {
+  e.stopPropagation();
+}
